e536aab8a0bc8c0c83ba0cc24492f34bfd4babff
slandeta <97185003+slandeta@users.noreply.github.com>
2022-10-13 18:07:47 -0400

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,5 +1,5 @@
-account_sid = "AC31f200148e54deaa32da4b6d6e0bb9d5"
-auth_token = "cb0f804554db626b174e5c75c47d5a5e"
+account_sid = [REDACTED]
+auth_token = [REDACTED]
 
 twilio_number = "+13466393660"
 target_number = "+13212660546"
